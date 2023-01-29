115f137e6125374557a6302b6a85cf9f0d2ed814
Marcos Alberto Sanmartín Pereira <ahriman89@gmail.com>
2023-01-29 23:28:28 +0100

    Added @Composable FabDialog and AddTasksDialog

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -30,7 +30,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    TasksScreen(tasksViewModel)
+//                    TasksScreen(tasksViewModel)
+                    TasksScreen()
                 }
             }
         }
--- app/src/main/java/com/example/todoapp/addtasks/ui/TasksScreen.kt
+++ app/src/main/java/com/example/todoapp/addtasks/ui/TasksScreen.kt
@@ -1,8 +1,72 @@
 package com.example.todoapp.addtasks.ui
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Preview
+@Composable
+fun TasksScreen(/*tasksViewModel: TasksViewModel*/) {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTasksDialog(true, onDismiss = {}, onTaskAdded = {}) {} // TODO:
+        FabDialog(Modifier.align(Alignment.BottomEnd))
+    }
+
+}
 
 @Composable
-fun TasksScreen(tasksViewModel: TasksViewModel){
+fun FabDialog(modifier: Modifier) {
+    FloatingActionButton(onClick = {
+        // Mostrar diálogo
+    }, modifier = modifier.padding(16.dp)) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription = "")
+    }
+}
 
+
+@Composable
+fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
+    var myTask by remember { mutableStateOf("") }
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Añade tú tarea",
+                    fontSize = 18.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                TextField(
+                    value = myTask,
+                    onValueChange = { myTask = it },
+                    singleLine = true,
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(onClick = {
+                    onTaskAdded(myTask)
+                }, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Añadir tarea")
+                }
+            }
+        }
+    }
 }
